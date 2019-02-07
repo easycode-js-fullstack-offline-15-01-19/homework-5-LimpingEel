37f59ef6012b5a3f8cee21a76f184e8409510c6b
LimpingEel <nik.doroshenko.00@ukr.net>
2019-02-07 21:24:39 +0200

    added task from presentation 6

--- homework.js
+++ homework.js
@@ -1,2 +1,48 @@
 // -------------------------- Home work --------------------------
-// -------------------------- ФИО --------------------------
+// -------------------------- Никита Дорошенко Андреевич --------------------------
+
+// DOM. Задачи.
+
+// // #1
+// console.log(document.head)
+
+// // #2
+// console.log(document.body)
+
+// // #3
+// console.log(document.body.childNodes)
+
+// // #4
+// const div = document.getElementById('first').childNodes;
+// console.log(div)
+
+// const firstDiv = document.getElementById('first');
+
+// firstDiv.removeChild(firstDiv.firstChild);
+// firstDiv.removeChild(firstDiv.lastChild);
+
+// console.log(firstDiv.childNodes)
+
+// DOM. Задачи.
+
+// #1
+// function isParent(parent, child) {
+// 	if (parent === child.closest('div')) {
+// 		return true
+// 	} else {
+// 		return false
+// 	}
+// }
+
+// #2
+// let links = Array.from(document.querySelectorAll('body a')).filter(link => !link.closest('ul'));
+// console.log(links);
+
+// #3
+console.log(document.querySelector('ul').previousSibling)
+console.log(document.querySelector('ul').nextSibling)
+
+// #4
+console.log(document.getElementsByTagName('li').length)
+
+// #5
